9aebd68b4e1ae597e76baf2760746ba7ec78ef37
Morales <116256085+Jmhegs@users.noreply.github.com>
2022-10-21 17:38:41 +0800

    Add files via upload

---
+++ frmConfirmation.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OrganizationProfile
+{
+    public partial class frmConfirmation : Form
+    {
+        public frmConfirmation()
+        {
+            InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void frmConfirmation_Load(object sender, EventArgs e)
+        {
+            lblStudentNo.Text = StudentInformationClass.SetStudentNo.ToString();
+            lblName.Text = StudentInformationClass.SetFullName;
+            lblProgram.Text = StudentInformationClass.SetProgram;
+            lblAge.Text = StudentInformationClass.SetBirthday;
+            lblBirthday.Text = StudentInformationClass.SetGender;
+            lblGender.Text = StudentInformationClass.SetContactNo.ToString();
+            lblContactNo.Text = StudentInformationClass.SetAge.ToString();
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblGender_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
