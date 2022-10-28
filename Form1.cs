ec85228d98e133394b5efc712817ae1ceec55429
Morales <116256085+Jmhegs@users.noreply.github.com>
2022-10-28 17:35:59 +0800

    Add files via upload

--- Form1.cs
+++ Form1.cs
@@ -5,99 +5,41 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Text.RegularExpressions;
 
-namespace OrganizationProfile
+namespace FrmBasicThread
 {
-    public partial class frmRegistration : Form
+    public partial class Form1 : Form
     {
-        private string _FullName;
-        private int _Age;
-        private long _ContactNo;
-        private long _StudentNo;
-
-        /////return methods 
-        public long StudentNumber(string studNum)
-        {
-
-            _StudentNo = long.Parse(studNum);
-
-            return _StudentNo;
-        }
-
-        public long ContactNo(string Contact)
-        {
-          
-            if (Regex.IsMatch(Contact, @"^[0-9]{10,11}$"))
-            {
-                _ContactNo = long.Parse(Contact);
-            }
-
-            return _ContactNo;
-        }
-
-        public string FullName(string LastName, string FirstName, string MiddleInitial)
-        {
-            if (Regex.IsMatch(LastName, @"^[a-zA-Z]+$") || Regex.IsMatch(FirstName, @"^[a-zA-Z]+$") || Regex.IsMatch(MiddleInitial, @"^[a-zA-Z]+$"))
-            {
-                _FullName = LastName + ", " + FirstName + ", " + MiddleInitial;
-            }
-
-            return _FullName;
-        }
-
-        public int Age(string age)
-        {
-            if (Regex.IsMatch(age, @"^[0-9]{1,3}$"))
-            {
-                _Age = Int32.Parse(age);
-            }
-
-            return _Age;
-        }
-
-        public frmRegistration()
+        public Form1()
         {
             InitializeComponent();
         }
 
-        private void label2_Click(object sender, EventArgs e)
-        {
-
-        }
-
         private void button1_Click(object sender, EventArgs e)
         {
-            StudentInformationClass.SetFullName = FullName(txtLastName.Text,
-           txtFirstName.Text, txtMiddleInitial.Text);
-            StudentInformationClass.SetStudentNo = StudentNumber(txtStudentNo.Text);
-            StudentInformationClass.SetProgram = cbPrograms.Text;
-            StudentInformationClass.SetGender = cbGender.Text;
-            StudentInformationClass.SetContactNo = ContactNo(txtContactNo.Text);
-            StudentInformationClass.SetAge = Age(txtAge.Text);
-            StudentInformationClass.SetBirthday = datePickerBirthday.Value.ToString("yyyy-MM - dd");
-            frmConfirmation frm = new frmConfirmation();
-            frm.ShowDialog();
-        }
+            Console.WriteLine("-Before starting thread-");
+            label1.Text = "-Before starting thread-";
+            ThreadStart Thread1 = new ThreadStart(MyThreadClass.Thread1);
+            Thread ThreadA = new Thread(Thread1);
+            Thread ThreadB = new Thread(Thread1);
 
-        private void frmRegistration_Load(object sender, EventArgs e)
-        {
-            string[] ListOfProgram = new string[]
-            {
-                "BS Infirmation Technology",
-                "BS Computer Science",
-                "BS Infirmation Systems",
-                "BS in Accountancy",
-                "BS in Hospitality Management",
-                "BS in Tourism Management"
-            };
-            for(int i = 0; i < 6; i++)
-            {
-                cbPrograms.Items.Add(ListOfProgram[i].ToString());
+            ThreadA.Name = "Thread A Process";
+            ThreadB.Name = "Thread B Process";
 
-            }
+            ThreadA.Priority = ThreadPriority.Highest;
+            ThreadB.Priority = ThreadPriority.Lowest;
+
+            ThreadA.Start();
+            ThreadB.Start();
+
+            ThreadA.Join();
+            ThreadB.Join();
+
+            Console.WriteLine("-End of Thread-");
+            label1.Text = "-End of Thread-";
         }
     }
 }
